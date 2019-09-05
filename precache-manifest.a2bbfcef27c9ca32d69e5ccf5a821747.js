a0618187762bbb6585b213ade354b98f9cec4b11
Trendy <trendy48@bk.ru>
2019-09-05 20:08:32 +0300

    Updates

---
+++ precache-manifest.a2bbfcef27c9ca32d69e5ccf5a821747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "510a25b9fa4f623c8c0b",
+    "url": "./static/js/main.46216a73.chunk.js"
+  },
+  {
+    "revision": "805ddc14871d574c597b",
+    "url": "./static/js/2.6b98f3cb.chunk.js"
+  },
+  {
+    "revision": "510a25b9fa4f623c8c0b",
+    "url": "./static/css/main.0521c3ae.chunk.css"
+  },
+  {
+    "revision": "805ddc14871d574c597b",
+    "url": "./static/css/2.700683d2.chunk.css"
+  },
+  {
+    "revision": "5a9eb970093625baad4636533cad50ce",
+    "url": "./index.html"
+  }
+];
